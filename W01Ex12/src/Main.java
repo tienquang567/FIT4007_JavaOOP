61bbc3179a61823961c0b48eafed76b7d9ae9722
tienquang567 <162654474+tienquang567@users.noreply.github.com>
2024-09-15 16:41:48 +0700

    Add files via upload

---
+++ W01Ex12/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+// varaiable
+// input
+        // process
+        int sum = 0;
+        int i ;
+        for (i = 7 ; i <= 100 ; i+=7) {
+            sum += i;
+        }
+      //out put
+        System.out.println(sum);
+    }
+}
