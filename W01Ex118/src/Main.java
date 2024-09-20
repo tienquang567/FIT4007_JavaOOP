10cee49c2156d9decbdee4555bb596a2f941c7cc
tienquang567 <162654474+tienquang567@users.noreply.github.com>
2024-09-20 18:16:44 +0700

    Add files via upload

---
+++ W01Ex118/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+//varaiable
+//input
+ //process
+        int i ;
+        for (  i = 300 ; i >= 3 ; i-- ){
+            if (i % 3 == 0 ){
+                //out
+                System.out.println( i );
+            }
+
+        }
+    }
+}
